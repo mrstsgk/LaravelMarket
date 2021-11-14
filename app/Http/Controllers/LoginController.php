d7284dcc0e81fb586f89f80d9c11a73239d4cf18
y-morita97 <yutarosu.39@gmail.com>
2021-11-14 14:35:24 +0900

    サービスクラスの作成・コード修正

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,35 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
-
+use App\Services\UserService;
 use App\Repositories\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
+    /** @Autowired */
+    private $userService;
+    
+    /**
+     * Serviceクラスの依存性を注入する.
+     *
+     * @param  mixed $userService
+     * @return void
+     */
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * ログインページ/マイページへの遷移.
+     *
+     * @return view
+     */
+    public function show() 
+    {
+        $userId = Cookie::get('userId');
+        if (isset($userId)) {
+            return redirect("/mypage");
+        } else {
+            return view('login', ['userName' => 'ゲスト']);
+        }
+    }
+
+    /**
+     * ログイン機能.
+     *
+     * @return view
+     */
     public function login(Request $request)
     {
-        // 入力値をUserテーブルで検索
-        $user = User::where('email', $request->email)->get();
+        $loginFlg = $this->userService->loginChk($request->email, $request->password);
 
         // なければエラーを返す
-        if (count($user) === 0){
-            return view('login', ['login_error' => '1']);
-        }
-        
-        // 一致
-        if (Hash::check($request->password, $user[0]->password)) {
-            
+        if ($loginFlg[0] === true){
             // cookie設定
-            Cookie::queue('userId', $user[0]->id, 60);
-
+            Cookie::queue('userId', $loginFlg[1], 60);
             return redirect(url('/'));
-        // 不一致    
-        }else{
+        } else {
             return view('login', ['login_error' => '1']);
         }
+
     } 
     
     public function logout(Request $request)
